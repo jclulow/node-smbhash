7c7b6cd15b9f8f630d0af58d8a073c89bb91b53e
Joshua M. Clulow <josh@sysmgr.org>
2011-11-12 16:16:31 +1100

    initial version

---
+++ README.md
@@ -0,0 +1,36 @@
+# node-smbhash: Samba LM/NT Hash Library
+
+## Introduction
+
+This library converts passwords into the LAN Manager (LM) and
+NT Hashes used by SMB/CIFS servers.  It was written to populate
+the sambaLMPassword and sambaNTPassword values in an LDAP directory
+for use with Samba.
+
+## Installation
+
+     npm install smbhash
+
+## Usage
+
+```javascript
+var lmhash = require('smbhash').lmhash;
+var nthash = require('smbhash').nthash;
+
+var pass = 'pass123';
+console.log('LM Hash: ' + lmhash(pass));
+console.log('NT Hash: ' + nthash(pass));
+```
+
+This produces output:
+
+```
+LM Hash: 4FB7D301186E0EB3AAD3B435B51404EE
+NT Hash: 5FBC3D5FEC8206A30F4B6C473D68AE76
+```
+
+## References
+
+     The NTLM Authentication Protocol and Security Support Provider
+     Copyright (C) 2003, 2006 Eric Glass
+     http://davenport.sourceforge.net/ntlm.html
---
+++ tests/smbhash.js
@@ -0,0 +1,51 @@
+/*
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABigIntegerLITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Copyright (C) 2011  Joshua M. Clulow <josh@sysmgr.org>
+ */
+
+var lmhash = require('../lib/smbhash').lmhash;
+var nthash = require('../lib/smbhash').nthash;
+
+var GOOD = [
+	{ password: 'pass123',
+	  lmhash: '4FB7D301186E0EB3AAD3B435B51404EE',
+	  nthash: '5FBC3D5FEC8206A30F4B6C473D68AE76' },
+	{ password: 'SecREt01',
+	  lmhash: 'FF3750BCC2B22412C2265B23734E0DAC',
+	  nthash: 'CD06CA7C7E10C99B1D33B7485A2ED808' }
+];
+
+module.exports.nthash_success = function(test) {
+	test.expect(GOOD.length * 2);
+	for (var i = 0; i < GOOD.length; i++) {
+		var g = GOOD[i];
+		test.doesNotThrow(function() {
+			var out = nthash(g.password);
+			test.strictEqual(out, g.nthash);
+		});
+	}
+	test.done();
+}
+
+module.exports.lmhash_success = function(test) {
+	test.expect(GOOD.length * 2);
+	for (var i = 0; i < GOOD.length; i++) {
+		var g = GOOD[i];
+		test.doesNotThrow(function() {
+			var out = lmhash(g.password);
+			test.strictEqual(out, g.lmhash);
+		});
+	}
+	test.done();
+}
